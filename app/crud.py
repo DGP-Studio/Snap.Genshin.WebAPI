3a973317a4197e592ceda0ae59bb2e1719f99219
Masterain <i@irain.in>
2022-04-16 00:04:08 -0700

    remove

--- app/crud.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-
-from . import models, schemas
-
-
-def getPluginID(db: Session, pluginName: str):
-    return db.query(models.Plugin).filter(models.Plugin.Name == pluginName).first()
-
